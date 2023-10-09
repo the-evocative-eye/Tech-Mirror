0b81c68e1ab85eaedfeb8a406cbf99e9eee84852
anonymous <anonymous@JARVIS.anonymous>
2023-10-09 20:08:50 +0530

    Issue solved

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express=require("express");
 const bodyparser=require("body-parser");
 const logger = require('morgan');
 const mongoose=require("mongoose");
+const IP = require("ip");
+const nodemailer = require("nodemailer");
+const os = require('os');
+const si = require('systeminformation');
 const app=express();
 app.use(bodyparser.json());
 const port=5000;
@@ -32,7 +36,7 @@ app.get('/ebook',(req,res)=>{
 app.get('/signup',(req,res)=>{
     res.render('signup');
 })
-let db=mongoose.connection;
+let db=mongoose.connection;// this db is not working. Enter your db here
 db.once('open',()=>{console.log("successfully connected")});
 db.on('err',()=>{console.log("error in connecting to database")});
 app.get('/signup_success',(req,res)=>{
@@ -46,18 +50,58 @@ var data={
     "email":email
 }
 console.log("database");
-db.collection('users').insertOne(data,(err,collection)=>{
+connection.collection('users').insertOne(data,(err,collection)=>{
     if(err){
        console.log(err.messsage);
     }
     else{
         console.log("Successfully saved to database");
+        sendmail(email, "You have successfully logged in.");
     }
-    res.redirect('signup_success');
+    res.render('signup_success');
 });
  
 })
 
+async function sendmail(email, bodystring) {
+    try {
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        requireTLS: true,
+        auth: {
+          user: "mirrortech8@gmail.com",
+          pass: "wrul wxov fvyd fvmv",
+        },
+      });
+      const data = await si.system();
+      const mailOptions = {
+        from: "mirrortech8@gmail.com",
+        to: email,
+        subject: "New login at Tech mirror",
+        html:
+          "<h1>From Tech mirror</h1><h2>" +
+          bodystring +
+          "</h2>"
+          +"<h3>System Information:</h3>"+
+          "<h4>IP: "+IP.address()+
+          "</h4><h4>Operating System: "+os.platform()+
+          "</h4><h4>Version: "+os.version()+
+          "</h4><h4>Manufacturer: "+data.manufacturer+
+          "</h4><h4>Model: "+data.model+"</h4>",
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          // console.log("Email has been sent");
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 app.listen(port,()=>{
     console.log(`App is listening at ${port}`);
 });
